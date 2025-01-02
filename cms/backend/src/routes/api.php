887ef28899ff203bce1af9062d3a4407fc722bc8
細川健一 <>
2025-01-02 16:42:53 +0900

    フロントエンドの設定

---
+++ cms/backend/src/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        return 'データベース接続成功';
+    } catch (\Exception $e) {
+        return 'データベース接続失敗: ' . $e->getMessage();
+    }
+});
