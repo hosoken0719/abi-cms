c94a436ad947f154daa6ce548c2b0307c154c64f
細川健一 <>
2025-01-02 23:49:21 +0900

    websiteを構築

---
+++ cms/website/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // basePath: '/website',  // 全てのパスに"/website"をプレフィックス
+  // assetPrefix: '/website', // 静的リソースにも"/website"をプレフィックス
+};
+
+export default nextConfig;
