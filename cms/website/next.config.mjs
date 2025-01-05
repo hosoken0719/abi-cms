8427b6f6675342c4a6493495778879ad64d94437
細川健一 <>
2025-01-05 22:08:14 +0900

    localとdevは完了

--- cms/website/next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: '/website',  // 全てのパスに"/website"をプレフィックス
-  // assetPrefix: '/website', // 静的リソースにも"/website"をプレフィックス
-};
-
-export default nextConfig;
